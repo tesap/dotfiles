32eeccf20e860bf943e2db02dc1593bbd3ca6949
tsepanx <tsx@riseup.net>
2021-05-01 13:03:20 +0300

    backup script

--- .config/blocks-dwmblocks.h
+++ .config/blocks-dwmblocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 // $HOME/.scripts/dwmblocks/
 
 {"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		2,		0},
-{"", "$HOME/.scripts/dwmblocks/load.sh",					2,		0}, // Load
+/* {"", "$HOME/.scripts/dwmblocks/load.sh",					2,		0}, // Load */
 // {"", "$HOME/.scripts/dwmblocks/uptime.sh",					60,		0},
 // {"Wifi: ", "$HOME/.scripts/dwmblocks/wifi.sh",			                30,		0},
 // {"Disk: ", "$HOME/.scripts/dwmblocks/disk.sh",				        15,		0},
