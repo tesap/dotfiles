068cd1c69fdd824bc6659bb0dd65e92c345a2291
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-10-29 14:30:34 +0300

    rm .scripts

--- .scripts/setup/install/virt-manager.sh
+++
@@ -1,7 +0,0 @@
-yay -S --needed qemu virt-manager libvirt dnsmasq ebtables bridge-utils
-
-enable="sudo systemctl enable --now"
-
-eval $enable "dnsmasq.service"
-eval $enable "libvirtd.service"
-eval $enable "ebtables.service"
--- .scripts/setup/install/virtualbox.sh
+++
@@ -1,6 +0,0 @@
-sudo pacman -S --needed virtualbox virtualbox-host-modules-arch linux-headers linux-lts-headers
-
-sudo gpasswd -a $USER vboxusers
-
-sudo modprobe vboxnetflt
-sudo modprobe vboxnetadp
